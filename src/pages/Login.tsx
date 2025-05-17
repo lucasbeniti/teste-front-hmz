228434b9d9db50d8d5b5cffd493b1122961e6e80
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-05-17 13:57:48 -0300

    feat: criando tela de login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import { Button } from "../components/ui/button";
+import { Card, CardContent } from "../components/ui/card";
+import { Input } from "../components/ui/input";
+
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="flex flex-col justify-center items-center bg-white p-8 w-full h-full">
+        <h1 className="text-8xl font-light text-gray-500 mb-4 px-50">
+          Simplificamos juntos
+        </h1>
+        <p className="text-md text-gray-400 absolute bottom-15">
+          Supply Chain | Industrial | Systems
+        </p>
+      </div>
+
+      <div className="bg-gray-200 p-6">
+        <div className="bg-white w-full h-full p-10">
+          <Card className="w-full bg-gray-100 rounded-md h-full justify-center relative">
+            <CardContent>
+              <div className="w-[50%] bg-white rounded-md p-10 text-center text-gray-400 mb-20 m-auto">
+                LOGO
+              </div>
+            
+              <p className="text-center text-gray-400 text-md mb-2 uppercase">Login</p>
+
+              <div className="flex flex-col gap-4 pb-20">
+                <Input 
+                  className="bg-white text-sm px-5"
+                  placeholder="EMAIL"
+                  type="email"
+                />
+
+                <Input 
+                  className="bg-white text-sm px-5"
+                  placeholder="SENHA"
+                  type="password"
+                />
+
+                <Button className="bg-white text-gray-400 hover:bg-white hover:text-gray-500 w-fit self-center px-10 uppercase">
+                  Logar
+                </Button>
+              </div>
+
+              <div className="flex justify-between items-center">
+                <Button variant={"ghost"} className="uppercase text-gray-400">
+                  Esqueci minha senha
+                </Button>
+
+                <Button variant={"ghost"} className="uppercase text-gray-400">
+                  Cadastre-se
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
+ 
+export default LoginPage;
